f6c1a56832d85e5e15099b912072b7db4fb5be99
xxl <1341531023@qq.com>
2022-07-08 19:14:50 +0800

    前端

--- src/main/java/com/yqp/controller/LogController.java
+++ src/main/java/com/yqp/controller/LogController.java
@@ -1,16 +1,21 @@
 package com.yqp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yqp.common.ResponseJson;
+import com.yqp.common.Utils;
 import com.yqp.domain.Log;
 import com.yqp.domain.Page;
 import com.yqp.service.LogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,17 +32,26 @@ public class LogController {
     @Resource
     private LogService logService;
 
+    @GetMapping("/toList")
+    public String toLogList(Model model, HttpServletRequest request){
+        String username = Utils.getSession(request, "username");
+        model.addAttribute("username",username);
+        return "logs";
+    }
+
     @GetMapping("/list")
-    public String getLogList(Page page, Model model){
+    @ResponseBody
+    public String getLogList(Page page, Model model) throws JsonProcessingException {
         List<Log> list = logService.getList(page);
 
+        ObjectMapper mapper = new ObjectMapper();
         Map<String,Object> map = new HashMap<>();
         map.put("code",0);
         map.put("message","");
         map.put("count",logService.getCount());
         map.put("data",list);
-        model.addAttribute("list",list);
-        return "logs";
+        //model.addAttribute("list",list);
+        return mapper.writeValueAsString(map);
     }
 
 
