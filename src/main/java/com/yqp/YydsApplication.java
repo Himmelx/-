cdc7790aa1b557493f6acac784d5954e70585807
qingpeng.yu <2847028305@qq.com>
2022-07-08 21:23:32 +0800

    后端

--- src/main/java/com/yqp/YydsApplication.java
+++ src/main/java/com/yqp/YydsApplication.java
@@ -3,13 +3,20 @@ package com.yqp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.yqp.mapper")
-public class YydsApplication {
+public class YydsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(YydsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(YydsApplication.class);
     }
 
 }
